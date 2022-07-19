73c07f8976cbf271c107220a19706020675ca6c8
Tapas Adhikary <tapas.adhikary@gmail.com>
2022-07-19 14:30:25 +0530

    Made Additional Style changes and linked the images

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,6 +4,8 @@ import {useStaticQuery, graphql} from 'gatsby';
 
 import './gallery.css';
 
+import { GitHub, Twitter, BookOpen } from 'react-feather';
+
 const Gallery = () => {
     const data = useStaticQuery(
         graphql`
@@ -27,17 +29,62 @@ const Gallery = () => {
     );
     const images = data.allCloudinaryMedia.edges;
     return (
-        <div className="container">
-            {images.map((image, index) => (
-                <figure className="wave" key={`${index}-image`}>
-                    <img 
-                        src={image.node.secure_url} 
-                        alt={image.node.context.custom.alt} >
-                    </img>
-                    <figcaption>{image.node.context.custom.caption}</figcaption>
-                </figure>
-                ))
-            }
+        <div>
+            <header>
+                <h1>Imaginary</h1>
+                <ul className="links">
+                    <li>
+                        <a 
+                            href="https://github.com/atapas/imaginary" 
+                            target="_blank"
+                            rel="noopener noreferrer">
+                                <GitHub color="#ffffff" />
+                        </a>
+                    </li>
+                    <li>
+                        <a 
+                            href="https://www.freecodecamp.org/news/how-to-create-an-image-gallery-gatsby-and-cloudinary/"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                                <BookOpen color="#ffffff" />
+                            </a>
+                    </li>
+                    <li>
+                        <a 
+                            href="https://twitter.com/tapasadhikary"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                                <Twitter color="#ffffff" />
+                        </a>
+                    </li>
+                </ul>
+            </header>
+            <div className="container">
+                {images.map((image, index) => (
+                    <a 
+                        href={encodeURI(`https://google.com/search?q=${image.node.context.custom.caption}`)}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        key={`${index}-image`}>
+                        <figure className="wave">
+                            <img 
+                                src={image.node.secure_url} 
+                                alt={image.node.context.custom.alt} >
+                            </img>
+                            <figcaption>{image.node.context.custom.caption}</figcaption>
+                        </figure>
+                    </a>
+                    ))
+                }
+            </div>
+            <footer>
+                <p>
+                    &copy; {new Date().getFullYear()} Imaginary. Made with ❤️ by <a 
+                    href="https://tapasadhikary.com"
+                    target="_blank"
+                    rel="noopener noreferrer">Tapas Adhikary</a>.
+                </p>
+            </footer>
         </div>
     )
 };
