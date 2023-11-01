7e64cc895aad0a11a3c376534a4768d0750b91ef
Moheshwar Amarnath Biswas <fluentmoheshwar@outlook.com>
2023-11-01 22:59:31 +0600

    stack: raw css to tailwind css

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -29,64 +29,76 @@ const Gallery = () => {
     );
     const images = data.allCloudinaryMedia.edges;
     return (
-        <div>
-            <header>
-                <h1>Imaginary</h1>
-                <ul className="links">
-                    <li>
-                        <a 
-                            href="https://github.com/atapas/imaginary" 
-                            target="_blank"
-                            rel="noopener noreferrer">
-                                <GitHub color="#ffffff" />
-                        </a>
-                    </li>
-                    <li>
-                        <a 
-                            href="https://www.freecodecamp.org/news/how-to-create-an-image-gallery-gatsby-and-cloudinary/"
-                            target="_blank"
-                            rel="noopener noreferrer">
-                                <BookOpen color="#ffffff" />
-                            </a>
-                    </li>
-                    <li>
-                        <a 
-                            href="https://twitter.com/tapasadhikary"
-                            target="_blank"
-                            rel="noopener noreferrer">
-                                <Twitter color="#ffffff" />
-                        </a>
-                    </li>
-                </ul>
-            </header>
-            <div className="container">
-                {images.map((image, index) => (
-                    <a 
-                        href={encodeURI(`https://google.com/search?q=${image.node.context.custom.caption}`)}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        key={`${index}-image`}>
-                        <figure className="wave">
-                            <img 
-                                src={image.node.secure_url} 
-                                alt={image.node.context.custom.alt} >
-                            </img>
-                            <figcaption>{image.node.context.custom.caption}</figcaption>
-                        </figure>
-                    </a>
-                    ))
-                }
-            </div>
-            <footer>
-                <p>
-                    &copy; {new Date().getFullYear()} Imaginary. Made with ❤️ by <a 
-                    href="https://tapasadhikary.com"
-                    target="_blank"
-                    rel="noopener noreferrer">Tapas Adhikary</a>.
-                </p>
-            </footer>
+      <div>
+        <header className="flex justify-between p-[5px] bg-black">
+          <h1 className="text-white text-3xl font-bold mr-1">Imaginary</h1>
+          <ul className="flex">
+            <li className="mr-[10px]">
+              <a
+                href="https://github.com/atapas/imaginary"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <GitHub color="#ffffff" />
+              </a>
+            </li>
+            <li className="mr-[10px]">
+              <a
+                href="https://www.freecodecamp.org/news/how-to-create-an-image-gallery-gatsby-and-cloudinary/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <BookOpen color="#ffffff" />
+              </a>
+            </li>
+            <li className="mr-[10px]">
+              <a
+                href="https://twitter.com/tapasadhikary"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Twitter color="#ffffff" />
+              </a>
+            </li>
+          </ul>
+        </header>
+        <div className="mt-14 flex flex-row flex-wrap content-center justify-center items-center">
+          {images.map((image, index) => (
+            <a
+              href={encodeURI(
+                `https://google.com/search?q=${image.node.context.custom.caption}`
+              )}
+              target="_blank"
+              rel="noopener noreferrer"
+              key={`${index}-image`}
+            >
+              <figure className="wave flex flex-col content-center justify-center items-center m-5 w-[200px]">
+                <img
+                  src={image.node.secure_url}
+                  alt={image.node.context.custom.alt}
+                  className='block w-[200px] h-[250px] bg-black'
+                ></img>
+                <figcaption className='text-center'>{image.node.context.custom.caption}</figcaption>
+              </figure>
+            </a>
+          ))}
         </div>
-    )
+        <footer className='flex justify-center p-[10px]'>
+          <p>
+            &copy; {new Date().getFullYear()} Imaginary. Made with ❤️ by{" "}
+            <a
+              href="https://tapasadhikary.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className='underline font-bold hover:no-underline'
+            >
+              Tapas Adhikary
+            </a>
+            .
+          </p>
+        </footer>
+      </div>
+    );
 };
 
 export default Gallery;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
